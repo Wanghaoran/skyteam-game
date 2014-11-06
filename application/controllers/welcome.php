ba5639a6a93c6d8fbdb3eb8d1390d23a5d6b3a72
WangHaoRan <whrlmc@139.com>
2014-11-07 03:16:28 +0800

    123123

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -46,25 +46,27 @@ class Welcome extends CI_Controller {
 
             if(isset($uid_get['error']) && $uid_get['error_code'] == 21321){
 
-                die('新浪微博登录功能正在等待微博方面审核，请稍后再试试');
+                $this->output->set_content_type('application/html;charset=utf-8') -> set_output('新浪微博登录功能正在等待微博方面审核，请稍后再试试');
+                exit;
 
             }else if(isset($uid_get['error']) && $uid_get['error_code'] != 21321){
 
-                die($uid_get['error']);
+                $this->output->set_content_type('application/html;charset=utf-8') -> set_output($uid_get['error']);
+                exit;
 
             }else{
 
                 $uid = $uid_get['uid'];
             }
         }else{
-            die('授权失败');
+            $this->output->set_content_type('application/html;charset=utf-8') -> set_output('授权验证失败！');
+            exit;
         }
 
 
 
 
         var_dump($_GET);
-        var_dump($uid_get);
         var_dump($uid);
     }
 
