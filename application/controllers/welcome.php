30d6ef1d6620c9d0af3bb50861a8756def8478e3
WangHaoRan <whrlmc@139.com>
2014-11-11 18:01:05 +0800

    123123213

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -83,6 +83,7 @@ class Welcome extends CI_Controller {
         //如果这个UID已经存在，则不能加入其他天团
         $this -> load -> model('user_model');
         if($this -> user_model -> getUser($this->session->userdata('token')['uid'])){
+            header("Content-type:text/html;charset=utf-8");
             echo '<script>alert("您已加入了其他天团，不能再加入别人的团啦！");location.href="http://skyteam.tianxun.cn/rank"</script>';
         }else{
             var_dump($uid);
